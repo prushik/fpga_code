513d43e92953640ace435a7857f0744e4564d4bb
Philip Rushik <prushik@gmail.com>
2017-12-02 20:35:41 -0500

    7seg converter needed to be inverted

--- src/examples/2.v
+++ src/examples/2.v
@@ -22,8 +22,8 @@ module example_2
 );
 
 reg [7:0] num = 7'h00;
-reg [7:0] seg_1;
-reg [7:0] seg_2;
+wire [7:0] seg_1;
+wire [7:0] seg_2;
 
 //wire w_segment1_a;
 //wire w_segment1_b;
@@ -43,15 +43,14 @@ reg [7:0] seg_2;
 
 always @(posedge i_clk)
 begin
-	num = 7'h00;
 	if (i_switch_1 == 1'b1)
 		num = num + 1;
 	if (i_switch_2 == 1'b1)
-		num = num + 2;
+		num = num | 1;
 	if (i_switch_3 == 1'b1)
-		num = num + 4;
+		num = num << 1;
 	if (i_switch_4 == 1'b1)
-		num = num + 8;
+		num = 7'h00;
 end
 
 bin_to_7seg inst
@@ -61,19 +60,20 @@ bin_to_7seg inst
 	.out_seg2(seg_2)
 );
 
-assign o_segment1_a = seg_1[0];
-assign o_segment1_b = seg_1[1];
-assign o_segment1_c = seg_1[2];
-assign o_segment1_d = seg_1[3];
-assign o_segment1_e = seg_1[4];
-assign o_segment1_f = seg_1[5];
-assign o_segment1_g = seg_2[6];
-assign o_segment2_a = seg_2[0];
-assign o_segment2_b = seg_2[1];
-assign o_segment2_c = seg_2[2];
-assign o_segment2_d = seg_2[3];
-assign o_segment2_e = seg_2[4];
-assign o_segment2_f = seg_2[5];
-assign o_segment2_g = seg_2[6];
+// Apparently, segments need to be driven low to be lit...
+assign o_segment1_a = ~seg_2[6];
+assign o_segment1_b = ~seg_2[5];
+assign o_segment1_c = ~seg_2[4];
+assign o_segment1_d = ~seg_2[3];
+assign o_segment1_e = ~seg_2[2];
+assign o_segment1_f = ~seg_2[1];
+assign o_segment1_g = ~seg_2[0];
+assign o_segment2_a = ~seg_1[6];
+assign o_segment2_b = ~seg_1[5];
+assign o_segment2_c = ~seg_1[4];
+assign o_segment2_d = ~seg_1[3];
+assign o_segment2_e = ~seg_1[2];
+assign o_segment2_f = ~seg_1[1];
+assign o_segment2_g = ~seg_1[0];
 
 endmodule
--- src/examples/bin_to_7seg.v
+++ src/examples/bin_to_7seg.v
@@ -8,42 +8,42 @@ module bin_to_7seg
 always @(*)
 begin
 	case(in_byte[3:0])
-		4'h0:    out_seg1 = 7'h7E;
-		4'h1:    out_seg1 = 7'h30;
-		4'h2:    out_seg1 = 7'h6D;
-		4'h3:    out_seg1 = 7'h79;
-		4'h4:    out_seg1 = 7'h33;          
-		4'h5:    out_seg1 = 7'h5B;
-		4'h6:    out_seg1 = 7'h5F;
-		4'h7:    out_seg1 = 7'h70;
-		4'h8:    out_seg1 = 7'h7F;
-		4'h9:    out_seg1 = 7'h7B;
-		4'ha:    out_seg1 = 7'h77;
-		4'hb:    out_seg1 = 7'h1F;
-		4'hc:    out_seg1 = 7'h4E;
-		4'hd:    out_seg1 = 7'h3D;
-		4'he:    out_seg1 = 7'h4F;
-		4'hf:    out_seg1 = 7'h47;
-		default: out_seg1 = 7'h00;
+		4'h0:    out_seg1 = 8'h7E;
+		4'h1:    out_seg1 = 8'h30;
+		4'h2:    out_seg1 = 8'h6D;
+		4'h3:    out_seg1 = 8'h79;
+		4'h4:    out_seg1 = 8'h33;
+		4'h5:    out_seg1 = 8'h5B;
+		4'h6:    out_seg1 = 8'h5F;
+		4'h7:    out_seg1 = 8'h70;
+		4'h8:    out_seg1 = 8'h7F;
+		4'h9:    out_seg1 = 8'h7B;
+		4'ha:    out_seg1 = 8'h77;
+		4'hb:    out_seg1 = 8'h1F;
+		4'hc:    out_seg1 = 8'h4E;
+		4'hd:    out_seg1 = 8'h3D;
+		4'he:    out_seg1 = 8'h4F;
+		4'hf:    out_seg1 = 8'h47;
+		default: out_seg1 = 8'h00;
 	endcase
 	case(in_byte[7:4])
-		4'h0:    out_seg2 = 7'h7E;
-		4'h1:    out_seg2 = 7'h30;
-		4'h2:    out_seg2 = 7'h6D;
-		4'h3:    out_seg2 = 7'h79;
-		4'h4:    out_seg2 = 7'h33;          
-		4'h5:    out_seg2 = 7'h5B;
-		4'h6:    out_seg2 = 7'h5F;
-		4'h7:    out_seg2 = 7'h70;
-		4'h8:    out_seg2 = 7'h7F;
-		4'h9:    out_seg2 = 7'h7B;
-		4'ha:    out_seg2 = 7'h77;
-		4'hb:    out_seg2 = 7'h1F;
-		4'hc:    out_seg2 = 7'h4E;
-		4'hd:    out_seg2 = 7'h3D;
-		4'he:    out_seg2 = 7'h4F;
-		4'hf:    out_seg2 = 7'h47;
-		default: out_seg2 = 7'h00;
+		4'h0:    out_seg2 = 8'h7E;
+		4'h1:    out_seg2 = 8'h30;
+		4'h2:    out_seg2 = 8'h6D;
+		4'h3:    out_seg2 = 8'h79;
+		4'h4:    out_seg2 = 8'h33;
+		4'h5:    out_seg2 = 8'h5B;
+		4'h6:    out_seg2 = 8'h5F;
+		4'h7:    out_seg2 = 8'h70;
+		4'h8:    out_seg2 = 8'h7F;
+		4'h9:    out_seg2 = 8'h7B;
+		4'ha:    out_seg2 = 8'h77;
+		4'hb:    out_seg2 = 8'h1F;
+		4'hc:    out_seg2 = 8'h4E;
+		4'hd:    out_seg2 = 8'h3D;
+		4'he:    out_seg2 = 8'h4F;
+		4'hf:    out_seg2 = 8'h47;
+		default: out_seg2 = 8'h00;
 	endcase
 end
 
